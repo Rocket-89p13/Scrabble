02905e6d1a1dd1ff1b8845319500128290f24448
tommy <jchung1230857@gmail.com>
2024-02-05 16:36:19 -0500

    Added Dictionary

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 
 from Player import Player
 from Tile import Tile
+from Dictionary import Dictionary
 
 # TODO
 # pass turn, exchange equal tile from the bag, game over
@@ -241,6 +242,7 @@ def render(screen, font, board, players, submit_button):
 
 def main():
     tile_values = {'a': 1,'b': 3,'c': 3,'d': 2,'e': 1,'f': 4,'g': 2,'h': 4,'i': 1,'j': 8,'k': 5,'l': 1,'m': 3,'n': 1,'o': 1,'p': 3,'q': 10,'r': 1,'s': 1,'t': 1,'u': 1,'v': 4,'w': 4,'x': 8,'y': 4,'z': 10,' ': 0}
+    dictionary = Dictionary()
     pygame.init()
     WIDTH, HEIGHT, ROW_LEN, NUM_PLAYERS = 800, 800, 15, 2
     tiles = init_tiles()
@@ -270,6 +272,7 @@ def main():
                 if (event.key == pygame.K_r):
                     reset_move()
         render(screen, font, board, players, button)
+    dictionary.close()
     pygame.quit()
     return
 
